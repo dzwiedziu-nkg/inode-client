23387eaf8abc3da312c58dff0059e06f44e7616c
nkg <nkg753@gmail.com>
2016-06-29 09:36:49 +0200

    Init: example app

---
+++ app/src/main/java/pl/nkg/iot/inode/example/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.nkg.iot.inode.example;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
